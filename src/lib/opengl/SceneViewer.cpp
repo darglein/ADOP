f0e36866873b03e2eb9517429c8d0fe2a1cc6af4
Darius Rueckert <darius.rueckert@fau.de>
2022-06-15 18:34:20 +0200

    update saiga

--- src/lib/opengl/SceneViewer.cpp
+++ src/lib/opengl/SceneViewer.cpp
@@ -46,11 +46,6 @@ SceneViewer::SceneViewer(std::shared_ptr<SceneData> scene) : scene(scene)
 
     {
         auto obj = FrustumLineMesh(scene_camera.proj, frustum_size, false);
-
-        for (auto p : obj.position)
-        {
-            std::cout << p.transpose() << std::endl;
-        }
         obj.SetVertexColor(vec4(1, 1, 1, 1));
         frustum_asset = std::make_shared<LineVertexColoredAsset>(obj);
     }
